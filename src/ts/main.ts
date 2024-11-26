e007f3e08ff3243f3d155a694f74518b8bd76f37
shahbazkhaliddev786 <shahbazkhalid818@gmail.com>
2024-11-26 23:15:26 +0500

    first commit

---
+++ src/ts/main.ts
@@ -0,0 +1,7 @@
+// navbar
+const hamburger = document.querySelector('.hamburger');
+const navbar = document.querySelector('.navbar');
+
+hamburger?.addEventListener('click',()=>{
+    navbar?.classList.toggle('toggle');
+});
